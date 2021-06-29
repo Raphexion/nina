c16817a868e71b135678b953a15f7f9c689a4acf
Niklas Johansson <raphexion@gmail.com>
2021-06-29 22:31:18 +0200

    cmd: Add entries command

---
+++ cmd/entries.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"nina/noko"
+
+	"github.com/spf13/cobra"
+)
+
+func NewEntryCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:     "entries",
+		Aliases: []string{"entry"},
+	}
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all entries",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := noko.NewClient()
+
+			ctx := context.Background()
+			entries, err := client.GetEntries(ctx, CurrentUser)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, entry := range entries {
+				fmt.Printf("%-10s %-80s %-10d\n", entry.User.FirstName, entry.Description, entry.Minutes)
+			}
+		},
+	}
+
+	rootCmd.AddCommand(listCmd)
+
+	return rootCmd
+}
