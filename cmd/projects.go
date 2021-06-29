b182539b6c4fb6883d7b2f6b9eae6592b906369b
Niklas Johansson <raphexion@gmail.com>
2021-06-29 21:10:40 +0200

    cmd: Add projects list command

---
+++ cmd/projects.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"nina/noko"
+
+	"github.com/spf13/cobra"
+)
+
+func NewProjectCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:     "projects",
+		Aliases: []string{"project"},
+	}
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all projects",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := noko.NewClient()
+
+			ctx := context.Background()
+			projects, err := client.GetProjects(ctx)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, project := range projects {
+				fmt.Printf("%-30s\n", project.Name)
+			}
+		},
+	}
+
+	rootCmd.AddCommand(listCmd)
+
+	return rootCmd
+}
