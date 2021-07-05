b12913615d03b32239115e2f076c0b002fb4404f
Niklas Johansson <raphexion@gmail.com>
2021-07-05 19:19:51 +0200

    cmd/timers: Improve readability

--- cmd/timers.go
+++ cmd/timers.go
@@ -19,142 +19,47 @@ func NewTimerCmd() *cobra.Command {
 	listCmd := &cobra.Command{
 		Use:   "list",
 		Short: "List all timers",
-		Run: func(cmd *cobra.Command, args []string) {
-			timers, err := mid.GetTimers()
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for _, timer := range timers {
-				minutes := timer.Seconds / 60
-				hours := minutes / 60
-				minutes -= hours * 60
-				fmt.Printf("%-30s %2dh%2d, %8s: %s\n", timer.Project.Name, hours, minutes, timer.State, timer.Description)
-			}
-		},
+		Run:   listCmdFunc,
 	}
 
 	pauseCmd := &cobra.Command{
 		Use:   "pause",
 		Short: "Pause active timer",
-		Run: func(cmd *cobra.Command, args []string) {
-			timer, err := mid.GetRunningTimer()
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err = mid.PauseTimer(timer); err != nil {
-				log.Fatal(err)
-			}
-
-			outputTimerWithName(timer.Project.Name)
-		},
+		Run:   pauseCmdFunc,
 	}
 
 	unpauseCmd := &cobra.Command{
 		Use:   "unpause [name of timer]",
 		Short: "Unpause a paused timer",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			name := strings.Join(args, "")
-			timer, err := mid.TimerWithName(name)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err = mid.StartTimer(timer); err != nil {
-				log.Fatal(err)
-			}
-
-			outputTimerWithName(timer.Project.Name)
-		},
+		Run:   unpauseCmdFunc,
 	}
 
 	noteCmd := &cobra.Command{
 		Use:   "note text",
 		Short: "Append a note for the running timer",
-		Run: func(cmd *cobra.Command, args []string) {
-			timer, err := mid.GetRunningTimer()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			var text string
-			if timer.Description == "" {
-				text = strings.Join(args, " ")
-			} else {
-				text = timer.Description + ". " + strings.Join(args, " ")
-			}
-
-			if err = mid.SetDescription(text); err != nil {
-				log.Fatal(err)
-			}
-
-			outputTimerWithName(timer.Project.Name)
-		},
+		Run:   noteCmdFunc,
 	}
 
 	createCmd := &cobra.Command{
 		Use:   "create [name of project]",
 		Short: "Create a timer for a project",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			name := strings.Join(args, " ")
-			timer, err := mid.CreateTimer(name)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			outputTimerWithName(timer.Project.Name)
-		},
+		Run:   createCmdFunc,
 	}
 
 	logCmd := &cobra.Command{
 		Use:   "log [name of the project]",
 		Short: "Log and finish timer for a given project",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			name := strings.Join(args, "")
-			timer, err := mid.TimerWithName(name)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err = mid.LogTimer(timer); err != nil {
-				log.Fatal(err)
-			}
-
-			timer.State = "finished"
-			outputTimer(timer)
-		},
+		Run:   logCmdFunc,
 	}
 
 	deleteCmd := &cobra.Command{
 		Use:   "delete [name of the project]",
 		Short: "Delete a timer",
 		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			name := strings.Join(args, " ")
-			timer, err := mid.TimerWithName(name)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			doDelete := promptForConfirmation(fmt.Sprintf("Are you sure you want to delete %s", timer.Project.Name))
-
-			if !doDelete {
-				return
-			}
-
-			if err = mid.DeleteTimer(timer); err != nil {
-				log.Fatal(err)
-			}
-
-			timer.State = "deleted"
-			outputTimer(timer)
-		},
+		Run:   deleteCmdFunc,
 	}
 
 	rootCmd.AddCommand(listCmd)
@@ -166,6 +71,115 @@ func NewTimerCmd() *cobra.Command {
 	rootCmd.AddCommand(deleteCmd)
 
 	return rootCmd
+}
+
+func listCmdFunc(cmd *cobra.Command, args []string) {
+	timers, err := mid.GetTimers()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, timer := range timers {
+		minutes := timer.Seconds / 60
+		hours := minutes / 60
+		minutes -= hours * 60
+		fmt.Printf("%-30s %2dh%2d, %8s: %s\n", timer.Project.Name, hours, minutes, timer.State, timer.Description)
+	}
+}
+
+func pauseCmdFunc(cmd *cobra.Command, args []string) {
+	timer, err := mid.GetRunningTimer()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = mid.PauseTimer(timer); err != nil {
+		log.Fatal(err)
+	}
+
+	outputTimerWithName(timer.Project.Name)
+}
+
+func unpauseCmdFunc(cmd *cobra.Command, args []string) {
+	name := strings.Join(args, "")
+	timer, err := mid.TimerWithName(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = mid.StartTimer(timer); err != nil {
+		log.Fatal(err)
+	}
+
+	outputTimerWithName(timer.Project.Name)
+}
+
+func noteCmdFunc(cmd *cobra.Command, args []string) {
+	timer, err := mid.GetRunningTimer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var text string
+	if timer.Description == "" {
+		text = strings.Join(args, " ")
+	} else {
+		text = timer.Description + ". " + strings.Join(args, " ")
+	}
+
+	if err = mid.SetDescription(text); err != nil {
+		log.Fatal(err)
+	}
+
+	outputTimerWithName(timer.Project.Name)
+}
+
+func createCmdFunc(cmd *cobra.Command, args []string) {
+	name := strings.Join(args, " ")
+	timer, err := mid.CreateTimer(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputTimerWithName(timer.Project.Name)
+}
+
+func logCmdFunc(cmd *cobra.Command, args []string) {
+	name := strings.Join(args, "")
+	timer, err := mid.TimerWithName(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = mid.LogTimer(timer); err != nil {
+		log.Fatal(err)
+	}
+
+	timer.State = "finished"
+	outputTimer(timer)
+}
+
+func deleteCmdFunc(cmd *cobra.Command, args []string) {
+	name := strings.Join(args, " ")
+	timer, err := mid.TimerWithName(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doDelete := promptForConfirmation(fmt.Sprintf("Are you sure you want to delete %s", timer.Project.Name))
+
+	if !doDelete {
+		return
+	}
+
+	if err = mid.DeleteTimer(timer); err != nil {
+		log.Fatal(err)
+	}
+
+	timer.State = "deleted"
+	outputTimer(timer)
 }
 
 // outputTimerWithName retreives the latest state of the timer
