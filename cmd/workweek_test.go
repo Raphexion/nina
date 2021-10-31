6116990cd6f162f10f647028433b31032ce38cc9
Niklas Johansson <raphexion@gmail.com>
2021-10-31 17:35:45 +0100

    cmd/workweek_test: Fix flaky timezone problem

--- cmd/workweek_test.go
+++ cmd/workweek_test.go
@@ -12,10 +12,12 @@ func TestWorkweekMonday(t *testing.T) {
 		mondayDateString string
 		sundayDateString string
 	}{
-		{"2011-01-19 01:02:03 +0100 CET", "Wednesday", "2011-01-17 00:00:00 +0100 CET", "2011-01-23 23:59:59 +0100 CET"},
+		{"2011-01-19 01:02:03", "Wednesday", "2011-01-17 00:00:00", "2011-01-23 23:59:59"},
+		{"2011-01-17 01:02:03", "Monday", "2011-01-17 00:00:00", "2011-01-23 23:59:59"},
+		{"2011-01-23 01:02:03", "Sunday", "2011-01-17 00:00:00", "2011-01-23 23:59:59"},
 	}
 
-	templateDate := "2006-01-02 15:04:05 -0700 MST"
+	templateDate := "2006-01-02 15:04:05"
 
 	for _, tt := range tests {
 		d, err := time.Parse(templateDate, tt.dateString)
@@ -33,12 +35,17 @@ func TestWorkweekMonday(t *testing.T) {
 			t.Fatalf("Unable to parse date string %s", tt.sundayDateString)
 		}
 
-		if Monday(d) != m {
+		if !equalDate(Monday(d), m) {
 			t.Fatalf("Incorrect Monday for %s. got: %s. expeced: %s", tt.dateString, Monday(d), tt.mondayDateString)
 		}
 
-		if Sunday(d) != s {
+		if !equalDate(Sunday(d), s) {
 			t.Fatalf("Incorrect Sunday for %s. got: %s. expeced: %s", tt.dateString, Sunday(d), tt.sundayDateString)
 		}
 	}
+
+}
+
+func equalDate(t1, t2 time.Time) bool {
+	return t1.Year() == t2.Year() && t1.YearDay() == t2.YearDay()
 }
